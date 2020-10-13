e11db29c7ac92b7e83c8b31471edb4e6c9191642
Mike Chen <517926804@qq.com>
2020-10-13 20:08:47 +0800

    优化启动脚本

--- startup.sh
+++ startup.sh
@@ -1,22 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 APP_HOME=`dirname $0`
 APP_HOME=`cd ${APP_HOME}; pwd`
 APP_LAUNCHER="$APP_HOME/tencent-token"
+APP_CONFIG="$APP_HOME/account.json"
 APP_PID_FILE="$APP_HOME/app.pid"
 
 APP_DAEMON_OUT="$APP_HOME/app.out"
 
-echo -e "wechat-token starting ... "
+echo -e "tencent-token starting ... "
 
 if [ -f $APP_PID_FILE ]; then
   if kill -0 `cat $APP_PID_FILE` > /dev/null 2>&1; then
-     echo wechat-token already running as process `cat $APP_PID_FILE`.
+     echo tencent-token already running as process `cat $APP_PID_FILE`.
      exit 0
   fi
 fi
 
-nohup $APP_LAUNCHER > $APP_DAEMON_OUT 2>&1 < /dev/null &
+nohup $APP_LAUNCHER -config $APP_CONFIG > $APP_DAEMON_OUT 2>&1 < /dev/null &
 
 if [ $? -eq 0 ]
 then
